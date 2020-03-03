07008298acada700796f178731444595e64ed8f2
Lindsay Montgomery <lindsay.montgomery@olympian.org>
2020-03-03 17:44:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,39 +2,44 @@
 
 require 'yaml'
 
-def load_library(file_path)
-emoticons = YAML.load_file(file_path)
-hash = {"get_meaning": {},
-       "get_emoticon": {},
-}
-emoticons.each do |key,value|
-  hash[:get_emoticon][value[0]] = value[1]
-  hash[:get_meaning][value[1]] = key.to_s
-  end
- hash
+def load_library(filepath)
+emoticons = YAML.load_file(filepath)
+
+  hash = {"get_meaning"=> {},
+  "get_emoticon"=> {}
+  }
+  emoticons.each do |key,value|
+      hash["get_meaning"][value[1]] = key
+      hash["get_emoticon"][value[0]] = value[1]
+    end 
+  hash
+end 
+
+def get_japanese_emoticon(file_path, emoticon)
+result = load_library(file_path)['get_emoticon'][emoticon]
+  result ? result : "Sorry, that emoticon was not found"
 end
 
-
-def get_japanese_emoticon(file_path, western_emoticon)
-  # code goes here
-result = "Sorry, that emoticon was not found"
- emoticons = load_library(file_path)
- emoticons[:get_emoticon].each do |key,value|
-   if key == western_emoticon
-    result = value
-   end
-  end
-  result
+def get_english_meaning(file_path, emoticon)
+result = load_library(file_path)['get_meaning'][emoticon]
+  result ? result : "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning(file_path, j_emoticon)
-  # code goes here
-  result = "Sorry, that emoticon was not found"
-  emoticons = load_library(file_path)
-  emoticons[:get_meaning].each do |key,value|
-    if key == j_emoticon
-      result = value
-    end
-  end
-  result
-end
+# def get_japanese_emoticon(filepath, eng_emoticon)
+# emoticons = load_library(filepath)
+# result = "Sorry, that emoticon was not found"
+#   emoticons["get_emoticon"].each do |key,value|
+#     result = value if key == eng_emoticon
+#   end
+# result
+# end
+
+# def get_english_meaning(filepath, jap_emoticon)
+# emoticons = load_library(filepath)
+# result = "Sorry, that emoticon was not found"
+#   emoticons["get_meaning"].each do |key,value|
+#     result = value if key == jap_emoticon
+#   end
+# result 
+# end
+
